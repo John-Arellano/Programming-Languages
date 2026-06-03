f4d47eb60ab7910ffd713311a53734f5da228a0d
John Arellano Maroto <j.stevea@gmail.com>
2026-06-03 13:43:23 -0600

    Greater_of_two numbers

---
+++ JavaScript/Greater_of_two numbers.js
@@ -0,0 +1,13 @@
+function greaterOfTwo(Num_1, Num_2) {
+  let A = Num_1;
+  let B = Num_2;
+  if (A > B) {
+    console.log("A is greater than B");
+  } else {
+    console.log("B is greater than A");
+  }
+}
+
+greaterOfTwo(5, 10);
+greaterOfTwo(20, 15);
+greaterOfTwo(7, 8);
