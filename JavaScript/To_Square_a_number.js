7aa47a5232111168f8f0c08627568e6555804ade
John Arellano Maroto <j.stevea@gmail.com>
2026-06-04 13:24:32 -0600

    To Square a number

---
+++ JavaScript/To_Square_a_number.js
@@ -0,0 +1,9 @@
+function squareNumber(numero) {
+  let n = numero;
+  let square = n * n;
+  console.log("The square of " + n + " is: " + square);
+}
+
+squareNumber(5);
+squareNumber(10);
+squareNumber(15);
