f124c4d8d15170ff7096f9ff2eec6977a13bafcd
John Arellano Maroto <j.stevea@gmail.com>
2026-06-09 13:46:50 -0600

    convertToLowercase

---
+++ JavaScript/Convert_text_to_lowercase.js
@@ -0,0 +1,8 @@
+function convertToLowercase(text) {
+  let result = text.toLowerCase();
+  console.log(result);
+}
+
+convertToLowercase("HELLO, WORLD!");
+convertToLowercase("JAVASCRIPT IS FUN!");
+convertToLowercase("PROGRAMMING LANGUAGES ARE GREAT!");
