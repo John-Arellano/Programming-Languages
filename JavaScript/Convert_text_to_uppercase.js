12e59d3f21cae5c9d638df64efddfd0e318e147d
John Arellano Maroto <j.stevea@gmail.com>
2026-06-09 13:36:06 -0600

    convertToUppercase

---
+++ JavaScript/Convert_text_to_uppercase.js
@@ -0,0 +1,8 @@
+function convertToUppercase(text) {
+  let result = text.toUpperCase();
+  console.log(result);
+}
+
+convertToUppercase("hello, world!");
+convertToUppercase("javascript is fun!");
+convertToUppercase("programming languages are great!");
