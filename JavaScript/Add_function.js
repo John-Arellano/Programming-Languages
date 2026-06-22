99386552fe702826ec45117e8c02c4aaac014d8d
John Arellano Maroto <j.stevea@gmail.com>
2026-06-22 13:21:29 -0600

    AddNumbers

---
+++ JavaScript/Add_function.js
@@ -0,0 +1,8 @@
+function AddNumbers(numa, numb) {
+  let result = numa + numb;
+  console.log("The sum of " + numa + " and " + numb + " is: " + result);
+}
+
+AddNumbers(5, 10);
+AddNumbers(20, 30);
+AddNumbers(100, 200);
